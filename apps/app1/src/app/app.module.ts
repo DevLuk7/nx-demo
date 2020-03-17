090997790c53d9a102129e2738a6a9237b0f3f38
Luk <lukass100@gmail.com>
2020-03-17 22:37:40 +0100

    updae html app1 and app2

--- apps/app1/src/app/app.module.ts
+++ apps/app1/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
+import { TestLibModule } from '@nx-demo/test-lib';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    TestLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
